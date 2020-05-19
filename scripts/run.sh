2c4831a7e2405f4a4254811c9dc5934b01da9738
Emanuel Donalds <emanueldonalds@edu.novia.fi>
2020-05-19 22:43:38 +0300

    Make sure java runs in the background

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
-java -jar "./../build/libs/snuskriget-server-1.0.0.jar"
+nohup java -jar "./../build/libs/snuskriget-server-1.0.0.jar"
